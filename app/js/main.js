730a84555c32c3d4e112e558f1b04801e7a9b489
chirkovv13 <patyomkin2011@mail.ru>
2023-10-02 13:14:06 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    // слфйдер
+  // слфйдер
   $('.top-slider__list').slick({
     dots: true,
     arrows: false,
@@ -9,7 +9,7 @@ $(function () {
     autoplaySpeed: 2000
   });
 
-    // рейтинг
+  // рейтинг
   $('.product-item__star').rateYo({
     readOnly: true,
     starWidth: '17px',
@@ -24,7 +24,7 @@ $(function () {
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
@@ -32,35 +32,63 @@ $(function () {
       minutes,
       seconds
     };
-  }
-  
+  };
+
   function initializeClock(id, endtime) {
     const clock = document.querySelector('.time');
     const daysSpan = clock.querySelector('.promo__days');
     const hoursSpan = clock.querySelector('.promo__hours');
     const minutesSpan = clock.querySelector('.promo__minutes');
     const secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
-      }
-    }
-  
+      };
+    };
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
-  }
-  
-  const deadline = $('.time').attr('data-time');
-  initializeClock('time', deadline);
+  };
+  $(function () { //если таймер не будет работать, то удалить фанкшн и скобки
+    const deadline = $('.time').attr('data-time');
+    initializeClock('time', deadline);
+  });
 
-    // ************
+  // ion.rangeSlider
+  $('.filter__price-input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter__price-from').text(data.from);
+      $('.filter__price-to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter__price-from').text(data.from);
+      $('.filter__price-to').text(data.to);
+    },
+  });
+
+  // styler
+  $('.select-style').styler();
+
+  //catalog__filter - grid > list > grid
+  $('.catalog__filter-btn').on('click', function () {
+    $('.catalog__filter-btn').removeClass('catalog__filter-btn--active');
+    $(this).addClass('catalog__filter-btn--active');
+  });
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  })
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  })
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,8 @@ const path           = {
             "node_modules/slick-carousel/slick/slick.js",
             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
             "node_modules/rateyo/src/jquery.rateyo.js",
+            "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+            "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
             pathSrc + "/js/main.js"
     ],
     watch:  [pathSrc + "/js/**/*.js", "!app/js/main.min.js"],
